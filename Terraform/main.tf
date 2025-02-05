21a6138318be69bb0ffa1dd52a09e84eed462719
AbdulrasheedApampa <apampaabdulrasheed16@gmail.com>
2025-02-05 08:52:12 +0100

    update

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,5 +1,16 @@
-module "terraform_state" {
-  source      = "./modules/terraform-state"
-  bucket_name = "rasheed-tp-bk"
-  table_name  = "my-dynamodb-table"
+# module "terraform_state" {
+#   source      = "./modules/terraform-state"
+#   bucket_name = "rasheed-tp-bk"
+#   table_name  = "my-dynamodb-table"
+# }
+
+
+terraform {
+  backend "s3" {
+    bucket         = "rasheed-tp-bk"
+    key            = "terraform.tfstate"  # Key path is just the state file name
+    region         = "us-east-1"
+    dynamodb_table = "my-dynamodb-table"
+    encrypt        = true
+  }
 }
